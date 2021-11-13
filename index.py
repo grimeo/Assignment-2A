67c852a8a3a193a9652b74afc75f0e3257df2b8d
Romeo <kilazlove@gmail.com>
2021-11-13 18:50:38 +0800

    init commit, input details and print

---
+++ index.py
@@ -0,0 +1,10 @@
+# Assignment 2A
+# Create a program that will ask for name, age and address. 
+# Display those details in the following format.
+# Hi, my name is ___. I am __ years old and I live in ___ .
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+address = input("Enter your address: ")
+
+print("Hi, my name is " + name + ". I am "+ age +" years old and I live in " + address +".")
